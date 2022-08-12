3c0501a93c284f7c2aab12e11c2c011837890159
ssc0126 <56520621+ssc0126@users.noreply.github.com>
2022-08-12 17:05:38 -0500

    s3 file upload success

--- Reddit API/s3_upload.py
+++ Reddit API/s3_upload.py
@@ -4,59 +4,77 @@ import configparser
 import pathlib
 import botocore
 from botocore.exceptions import ClientError
+import sys
+
+
 config_file = "my_config.ini"
-config_file_path = os.getcwd()#pathlib.Path(__file__).parent.absolute()
-#print(config_file_path)
+config_file_path = pathlib.Path(__file__).parent.absolute()
+print(config_file_path)
 config = configparser.ConfigParser()
-config.read(f"{config_file_path}\{config_file}")
+config.read(f"{config_file_path}/{config_file}")
+
+
 ACCESS_KEY = config.get('AWS', 'aws_access_key_id')
 SECRET_KEY = config.get('AWS', 'aws_secret_access_key')
 BUCKET_NAME = config.get('AWS', 'bucket_name')
+
+
+try:
+  FILENAME = sys.argv[1]
+except Exception as e:
+  print(f"Please input csv file name as cmd line arguemnt. Error {e}")
+  sys.exit(1)
+
+
+#FILENAME = f"{cmd_arg}"
+
+
 def s3_service_conn():
     try:
-        client = boto3.('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+        client = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key= SECRET_KEY)
         return client
     except:
         print('s3 connection error')
 
-def upload_file(connection, file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
 
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-
-    # Upload the file
-    s3_client = boto3.client('s3')
+def make_bucket(connection):
+    #https://boto3.amazonaws.com/v1/documentation/api/latest/guide/error-handling.html
+    bucket_exists = True
     try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-connection  =  s3_service_conn()
-#https://boto3.amazonaws.com/v1/documentation/api/latest/guide/error-handling.html
-bucket_exists = True
-try:
-    #checks if bucket already exists 
-    connection.head_bucket(Bucket = BUCKET_NAME)
-    
-except ClientError as err:
-     error = err.response['Error']['Code']
-     print(error)
-     if error != 200:
-        bucket_exist = False
+        #checks if bucket already exists 
+        connection.head_bucket(Bucket = BUCKET_NAME)
         
-if bucket_exists == False:
-    try:
-        connection.create_bucket(Bucket = BUCKET_NAME, CreateBucketConfiguration={'LocationConstraint': 'us-east-2'})
-
     except ClientError as err:
-        print('Could not create bucket')
+        error = err.response['Error']['Code']
+        if error != '200':
+            print(f'{err} \n')
+            bucket_exists = False
+            
+    if bucket_exists == False:
+        try:
+            connection.create_bucket(Bucket = BUCKET_NAME, CreateBucketConfiguration={'LocationConstraint': 'us-east-2'})
+            print('Success')
 
+        except ClientError as err:
+            err2 = err.response['Error']['Code']
+            print(f'{err2} \n')
+            print('Could not create bucket')
+            print('\n')
+
+def upload_file_to_s3(connection):
+    path = pathlib.Path(__file__).parent.absolute()
+    #print(path)
+    try:
+        connection.upload_file(Filename = f"{path}/FileDump/{FILENAME}", Bucket = BUCKET_NAME, Key = FILENAME)
+    except  ClientError as err:
+        print(f"upload failute. {err}")
+
+
+def main():
+    connection  =  s3_service_conn()
+    bucket = make_bucket(connection)
+    upload_file_to_s3(connection)
+
+
+if __name__ == '__main__':
+  main()
